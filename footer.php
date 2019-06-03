ce84f7d2bafcad15798e4a9587f090660745fdcf
Donaël Walt <w.dodo.a@hotmail.fr>
2019-06-03 10:52:59 +0200

    meny add + penser à ajouter wpForm sur le site finale

--- footer.php
+++ footer.php
@@ -3,14 +3,21 @@
 
 </div>
 <script type="text/javascript">
- var menu = document.querySelector('.menu ul');
- let burger = document.querySelector('#menu>#burger');
+ var menu = document.querySelector('.menu-item');
+ let menuChecker = document.querySelector('#menu>.menu-checker');
+ let cross = document.querySelector('#menu>.menu-checker>#cross');
+ let burger = document.querySelector('#menu>.menu-checker>#burger');
+ cross.style.display = "none";
  menu.style.display = "none";
- burger.addEventListener("click",function(){
+ menuChecker.addEventListener("click",function(){
    if(menu.style.display == "none"){
      menu.style.display = "flex";
+     cross.style.display = "flex";
+     burger.style.display = "none";
    }else if(menu.style.display == "flex"){
      menu.style.display = "none";
+     cross.style.display = "none";
+     burger.style.display = "flex";
    }
    else {
      menu.style.opacity = "none";
--- functions.php
+++ functions.php
@@ -2,4 +2,9 @@
     add_theme_support( 'post-thumbnails' );
 }
 add_action( 'after_setup_theme', 'mytheme_post_thumbnails' );
+
+function register_my_menu() {
+  register_nav_menu('menu_principal',__( 'Menu Principal' ));
+}
+add_action( 'init', 'register_my_menu' );
  ?>
