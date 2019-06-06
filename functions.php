dc3b889241a5910c263f4c9c6d5363d82584c424
Donaël Walt <w.dodo.a@hotmail.fr>
2019-06-06 17:18:38 +0200

    réecriture fonctions php

--- functions.php
+++ functions.php
@@ -1,10 +1,23 @@
-<?php function mytheme_post_thumbnails() {
-    add_theme_support( 'post-thumbnails' );
-}
-add_action( 'after_setup_theme', 'mytheme_post_thumbnails' );
+<?php
+function pierregrange_setup(){
+  /**
+  * Ajoute le support de la balise title
+  * Ajoute le support des images à la Une
+  * Ajoute le support du logo
+  **/
+  add_theme_support('title-tag');
+  add_theme_support( 'post-thumbnails' );
+  add_theme_support( 'custom-logo', array(
+    'height'      => 100,
+    'width'       => 400,
+    'flex-height' => true,
+    'flex-width'  => true,
+    'header-text' => array( 'site-title', 'site-description' ),
+) );
 
-function register_my_menu() {
+  /**
+  * Ajout du support de menu
+  **/
   register_nav_menu('menu_principal',__( 'Menu Principal' ));
 }
-add_action( 'init', 'register_my_menu' );
- ?>
+add_action('after_setup_theme', 'pierregrange_setup');
